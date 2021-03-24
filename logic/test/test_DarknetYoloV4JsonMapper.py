544f6993852c03e50bf17f0929d992826f6984f7
Normandi <ntr840@inlumine.ual.es>
2021-03-24 12:59:18 +0100

    Modifying paths for testing

--- logic/test/test_DarknetYoloV4JsonMapper.py
+++ logic/test/test_DarknetYoloV4JsonMapper.py
@@ -1,6 +1,6 @@
 # Test of mapping a JSON obtained with Darknet (YoloV4)
 import sys, os
-sys.path.append('d:\\Back-end\\Django y Python\\Python_code\\GUI_TFM_Normandi\\PythonGUI\\logic\\json_darknet_mapper')
+sys.path.append('c:\\Users\\Normandi\\darknet\\ThermalComfortGUI\\PythonGUI\\logic\\json_darknet_mapper')
 print(sys.path)
 print("Testing OS")
 newPath = sys.path[0]
@@ -11,6 +11,6 @@ from frame import DarknetYoloV4JsonMapper, Frame, ObjectFromFrame
 
 # Testing
 print('TESTING MAPPING OF JSON FROM DARKNET (YOLOV4)')
-pathToJsonYolov4 = 'PythonGUI\\logic\\test\\images\\image1.json'
+pathToJsonYolov4 = 'c:\\Users\\Normandi\\darknet\\ThermalComfortGUI\\PythonGUI\\logic\\test\\images\\image1.json'
 listOfFramesForTest = DarknetYoloV4JsonMapper.getFramesFromJSON(pathToJsonYolov4)
 DarknetYoloV4JsonMapper.printFrames(listOfFramesForTest)
